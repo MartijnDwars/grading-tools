205635f4079b8248bc47e1bbf9b3a295e8dc6b12
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-09-17 19:13:13 +0200

    extract method

--- reporter/src/org/metaborg/spt/listener/grading/Runner.java
+++ reporter/src/org/metaborg/spt/listener/grading/Runner.java
@@ -7,21 +7,21 @@ import org.apache.commons.io.FileUtils;
 
 public class Runner {
 
-	/**
-	 * Example of how to use this class.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
+		runTests("/Users/guwac/Cloud/git/in4303/grading/lab2/tests/");
+	}
+
+	public static void runTests(String project) {
+	
 		try {
-			FileUtils.deleteDirectory(new File("/Users/guwac/Cloud/git/in4303/grading/lab2/tests/.cache"));
+			FileUtils.deleteDirectory(new File(project+".cache"));
 		} catch (IOException e) {}
 	 
 		org.metaborg.sunshine.drivers.Main.main(
 			new String[] { 
 				"--auto-lang", "/Users/guwac/Cloud/git/in4303/grading/lab1/grammars/MiniJava-correct",
-				"--project", "/Users/guwac/Cloud/git/in4303/grading/lab2/tests/",
+				"--project", project,
 				"--builder", "testrunnerfile", "--build-on-all", ".", "--no-analysis"//, "--non-incremental"
 			});
 	}
