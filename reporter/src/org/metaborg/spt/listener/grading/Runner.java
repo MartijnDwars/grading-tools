911fddc8e874b9de3a74b4f61476cc89b0e5f6d8
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-09-17 20:16:42 +0200

    now inline esv discovery

--- reporter/src/org/metaborg/spt/listener/grading/Runner.java
+++ reporter/src/org/metaborg/spt/listener/grading/Runner.java
@@ -1,14 +1,27 @@
 package org.metaborg.spt.listener.grading;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PushbackInputStream;
 import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import org.apache.commons.io.FileUtils;
 import org.metaborg.sunshine.Environment;
 import org.metaborg.sunshine.drivers.SunshineMainArguments;
 import org.metaborg.sunshine.drivers.SunshineMainDriver;
+import org.metaborg.sunshine.services.language.ALanguage;
 import org.metaborg.sunshine.services.language.LanguageDiscoveryService;
+import org.metaborg.sunshine.services.language.LanguageService;
+import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.terms.TermFactory;
+import org.spoofax.terms.io.binary.TermReader;
 
 public class Runner {
 
@@ -24,18 +37,35 @@ public class Runner {
 		} catch (IOException e) {}
 	 
 		SunshineMainArguments params = new SunshineMainArguments();
-		params.autolang = language;
-		params.project = project;
 		params.builder = "testrunnerfile";
 		params.filestobuildon = ".";
 		params.noanalysis = true;
-		params.validate();
 		
 		Environment env = Environment.INSTANCE();
 		env.setMainArguments(params);
 		env.setProjectDir(new File(project));
-		LanguageDiscoveryService.INSTANCE().discover(
-					FileSystems.getDefault().getPath(language));
+		
+		Path locationPath = FileSystems.getDefault().getPath(language);
+		assert locationPath != null;
+		File location = locationPath.toFile();
+		if (!location.exists() || !location.isDirectory() || !location.canRead()) {
+			throw new RuntimeException(
+					"Language source location is does not exist, is not a directory or cannot be read: "
+							+ location.getAbsolutePath());
+		}
+		
+		Collection<ALanguage> languages = new LinkedList<ALanguage>();
+		
+		Iterator<File> esvs = FileUtils.iterateFiles(location, new String[] { "packed.esv" }, true);
+		while (esvs.hasNext()) {
+			try {
+				ALanguage language1 = readFromESV(esvs.next());
+				languages.add(language1);
+			} catch (IOException e) {
+				throw new RuntimeException("Failed to load language", e);
+			}
+		}
+		LanguageService.INSTANCE().registerLanguage(languages);
 		
 //		LanguageService.INSTANCE().registerLanguage(
 //					LanguageDiscoveryService.INSTANCE().languageFromArguments(params.languageArgs));
@@ -45,5 +75,24 @@ public class Runner {
 		} else {
 			System.exit(1);
 		}
+	}
+
+	public static ALanguage readFromESV(File esv)
+			throws FileNotFoundException, IOException {
+		
+		IStrategoAppl document = null;
+
+		PushbackInputStream input = new PushbackInputStream(new FileInputStream(esv), 100);
+		byte[] buffer = new byte[6];
+		int bufferSize = input.read(buffer);
+		if (bufferSize != -1)
+			input.unread(buffer, 0, bufferSize);
+		
+		if ((bufferSize == 6 && new String(buffer).equals("Module"))) {
+			TermReader reader = new TermReader(
+					new TermFactory().getFactoryWithStorageType(IStrategoTerm.MUTABLE));
+			document = (IStrategoAppl) reader.parseFromStream(input);
+		} 
+		return LanguageDiscoveryService.INSTANCE().languageFromEsv(document, esv.toPath().getParent().getParent());
 	}
 }
