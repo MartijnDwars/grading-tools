870d1620ff90459f79138d683d32ea9477ad6738
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-09-18 01:50:57 +0200

    run groups recursively

--- reporter/src/org/metaborg/spt/listener/grading/Runner.java
+++ reporter/src/org/metaborg/spt/listener/grading/Runner.java
@@ -7,6 +7,7 @@ import java.io.PushbackInputStream;
 
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.io.FileUtils;
@@ -23,29 +24,23 @@ import org.spoofax.terms.io.binary.TermReader;
 
 public class Runner {
 
+	final static CompositeConfiguration testConfig = new CompositeConfiguration();
+	
 	public static void main(String[] args) {
 
-		String project = "/Users/guwac/Cloud/git/in4303/grading/lab1/";
 		
 		int i = 0;
 		try {
-			CompositeConfiguration testConfig = new CompositeConfiguration();
-			testConfig.addConfiguration(new PropertiesConfiguration(project + "spt.properties"));
-			testConfig.addConfiguration(new PropertiesConfiguration(project + "tests.properties"));
-			
-			XMLConfiguration langConfig = new XMLConfiguration(project + "languages.xml");
-			
+			testConfig.addConfiguration(new PropertiesConfiguration("spt.properties"));
+			testConfig.addConfiguration(new PropertiesConfiguration("tests.properties"));
+						
 			register(testConfig.getString("spt.esv"));
-			String lang = langConfig.getString("language[@esv]");			
+			final String tests = testConfig.getString("tests");
+			final String builder = testConfig.getString("spt.builder");
+
+			XMLConfiguration langConfig = new XMLConfiguration("languages.xml");
 			
-			System.out.println(project + lang);
-			i += runTests(project + lang, project + testConfig.getString("tests"), testConfig.getString("spt.builder")); 
-			
-			for (Object variant: langConfig.getList("group.group.language[@esv]")) {
-				System.out.println(project + variant);
-				i += runTests(project + lang, project + testConfig.getString("tests"), testConfig.getString("spt.builder")); 
-			} 
-			
+			i += runGroup(langConfig.getFile().getParentFile().getPath(), langConfig, tests, builder); 
 			
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
@@ -54,7 +49,23 @@ public class Runner {
 		
 		System.exit(i);
 	}
-
+	
+	public static int runGroup(String project, HierarchicalConfiguration config,
+			final String tests, final String builder) {
+		
+		int i = 0;
+		for (Object variant: config.getList("language[@esv]")) {
+			System.out.println(project + variant);
+			i += runTests(project + "/" + variant, tests, builder); 
+		}
+		int g = 0;
+		for (Object group: config.getList("group[@name]")) {
+			System.out.println("Group " + group);
+			i += runGroup(project, config.configurationAt("group(" + g++ +")"), tests, builder);
+		}
+		return i;
+	}
+	
 	public static int runTests(String language, String project, String builder) {
 	
 		try {
@@ -71,16 +82,14 @@ public class Runner {
 		env.setProjectDir(new File(project));
 		
 		register(language);
-	
 		
 //		LanguageService.INSTANCE().registerLanguage(
 //					LanguageDiscoveryService.INSTANCE().languageFromArguments(params.languageArgs));
 		
-		if (new SunshineMainDriver().run() == 0) {
+		if (new SunshineMainDriver().run() == 0)
 			return 0;
-		} else {
+		else 
 			return 1;
-		}
 	}
 
 	public static void register(String esv) {
