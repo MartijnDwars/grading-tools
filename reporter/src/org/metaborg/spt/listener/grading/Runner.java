b92b990381a81e8a36b9184a050845478dd84d27
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-09-18 15:32:27 +0200

    markdown output to console

--- reporter/src/org/metaborg/spt/listener/grading/Runner.java
+++ reporter/src/org/metaborg/spt/listener/grading/Runner.java
@@ -30,6 +30,7 @@ public class Runner {
 	public static void main(String[] args) {
 	
 		int i = 0;
+		final StringBuilder report = new StringBuilder();
 		final int[] detection = new int[]{0, 0};
 		
 		try {
@@ -38,14 +39,14 @@ public class Runner {
 			final XMLConfiguration langConfig         = new XMLConfiguration("languages.xml");
 
 			final String sptDir   = sptConfig.getFile().getParentFile().getPath() + "/";
-			final String testsDir = testsConfig.getFile().getParentFile().getPath() + "/";
+//			final String testsDir = testsConfig.getFile().getParentFile().getPath() + "/";
 			final String langDir  = langConfig.getFile().getParentFile().getPath() + "/";
 			
 			register(sptDir + sptConfig.getString("spt.esv"));
-			final String tests   = testsDir + testsConfig.getString("tests");
+			final String tests   = testsConfig.getString("tests");
 			final String builder = sptConfig.getString("spt.builder");
 
-			i += runGroup(langDir, langConfig, tests, builder, detection); 
+			i += runGroup(report, "# ", langDir, langConfig, tests, builder, detection); 
 			
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
@@ -58,56 +59,73 @@ public class Runner {
 				Grader grader = (Grader) reporter;
 				final int valid = grader.getValid();
 				final int effective = grader.getEffective();
-				System.out.println("valid " + valid);
-				System.out.println("invalid " + grader.getInvalid());
-				System.out.println("effective " + effective);
-				System.out.println("ineffective " + (valid-effective));
-				System.out.println("detected " + detection[0]);
-				System.out.println("undetected " + detection[1]);
+				report.append("## Summary");
+				report.append(System.getProperty("line.separator"));
+				report.append(System.getProperty("line.separator"));
+				report.append("You have " + valid + " valid tests.");
+				report.append(System.getProperty("line.separator"));
+				report.append("You have " + grader.getInvalid() + " invalid tests.");
+				report.append(System.getProperty("line.separator"));
+				report.append(effective + " of your valid tests detected erroneous grammars.");
+//				report.append(System.getProperty("line.separator"));
+//				report.append("You detected " + detection[0] + " erroneous grammars.");
+//				report.append(System.getProperty("line.separator"));
+//				report.append("You missed " + detection[1] + " erroneous grammars.");
+//				report.append(System.getProperty("line.separator"));
 			}
 		}
 		
+		System.out.println(report.toString());
 		System.exit(i);
 	}
 	
-	public static int runGroup(String project, HierarchicalConfiguration config,
+	public static int runGroup(StringBuilder report, String header, String project, HierarchicalConfiguration config,
 			final String tests, final String builder, final int[] detection) {
 		
 		int i = 0;
 		for (String variant: config.getStringArray("language[@esv]")) {
-			for (Iterator<ITestReporter> reporters = TestReporterProvider.getInstance().getReporters(); reporters.hasNext();) {
-				ITestReporter reporter = reporters.next();
-				if (reporter instanceof Grader) {
-					Grader grader = (Grader) reporter;
-					grader.setLanguage(variant);
-				}
-			}
-			
-			i += runTests(project + variant, tests, builder); 
-			
-			for (Iterator<ITestReporter> reporters = TestReporterProvider.getInstance().getReporters(); reporters.hasNext();) {
-				ITestReporter reporter = reporters.next();
-				if (reporter instanceof Grader) {
-					Grader grader = (Grader) reporter;
-					if (grader.isDetected())
-						detection[0]++;
-					else detection[1]++;
-				}
-			}
+			i += runVariant(project, tests, builder, detection, variant);
 		}
 		
 		int g = 0;
 		for (Object group: config.getList("group[@name]")) {
+			
+			reportGroupHeader(report, header + group);
 			final int[] inner = new int[]{0, 0};
-			i += runGroup(project, config.configurationAt("group(" + g++ +")"), tests, builder, inner);
+			final StringBuilder subreport = new StringBuilder();
+			i += runGroup(subreport, "#" + header, project, config.configurationAt("group(" + g++ +")"), tests, builder, inner);
 			detection[0] += inner[0];
 			detection[1] += inner[1];
-			System.out.println("You detected " + inner[0] + " erroneous grammars for " + group);
-			System.out.println("You missed " + inner[1] + " erroneous grammars for " + group);
+			reportGroupSuccess(report, subreport, group.toString(), detection, inner);
 		}
 		return i;
 	}
-	
+
+	private static int runVariant(String project, final String tests,
+			final String builder, final int[] detection, String variant) {
+		
+		for (Iterator<ITestReporter> reporters = TestReporterProvider.getInstance().getReporters(); reporters.hasNext();) {
+			ITestReporter reporter = reporters.next();
+			if (reporter instanceof Grader) {
+				Grader grader = (Grader) reporter;
+				grader.setLanguage(variant);
+			}
+		}
+		
+		int i = runTests(project + variant, tests, builder); 
+		
+		for (Iterator<ITestReporter> reporters = TestReporterProvider.getInstance().getReporters(); reporters.hasNext();) {
+			ITestReporter reporter = reporters.next();
+			if (reporter instanceof Grader) {
+				Grader grader = (Grader) reporter;
+				if (grader.isDetected())
+					detection[0]++;
+				else detection[1]++;
+			}
+		}
+		return i;
+	}
+
 	public static int runTests(String language, String project, String builder) {
 	
 		try {
@@ -157,4 +175,77 @@ public class Runner {
 			throw new RuntimeException("Failed to load language", e); 
 		}
 	}
+	
+	private static void reportGroupHeader(StringBuilder report, String header) {
+		report.append(header);
+		report.append(System.getProperty("line.separator"));
+		report.append(System.getProperty("line.separator"));
+	}
+	
+	private static void reportGroupSuccess(StringBuilder report, StringBuilder subreport, String group, final int[] detection, final int[] inner) {
+		
+		final int detected = detection[0];
+		final int missed   = detection[1];
+		if (detected == 0) {
+			report.append("You missed all erroneous grammars.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			return;
+		}
+		if (missed == 0) {
+			report.append("You detected all erroneous grammars.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			return;
+		}
+		
+		if (detected/missed >= 3.0) {
+			report.append("You detected many erroneous grammars.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			if (inner[0] > 0) {
+				report.append(subreport);
+				report.append(System.getProperty("line.separator"));
+				report.append(System.getProperty("line.separator"));
+			}
+			return;
+		}
+		
+		if (detected > missed) {
+			report.append("You detected more erroneous grammars than you missed.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			if (inner[0] > 0) {
+				report.append(subreport);
+				report.append(System.getProperty("line.separator"));
+				report.append(System.getProperty("line.separator"));
+			}
+			return;			
+		}
+		
+		if (missed/detected >= 3.0) {
+			report.append("You missed many erroneous grammars.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			if (inner[0] > 0) {
+				report.append(subreport);
+				report.append(System.getProperty("line.separator"));
+				report.append(System.getProperty("line.separator"));
+			}
+			return;
+		}
+		
+		if (missed > detected) {
+			report.append("You missed more erroneous grammars than you detected.");
+			report.append(System.getProperty("line.separator"));
+			report.append(System.getProperty("line.separator"));
+			if (inner[0] > 0) {
+				report.append(subreport);
+				report.append(System.getProperty("line.separator"));
+				report.append(System.getProperty("line.separator"));
+			}
+			return;			
+		}
+	}
+
 }
