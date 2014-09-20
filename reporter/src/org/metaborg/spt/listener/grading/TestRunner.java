0eb4a175d5fe121853df62382e1a6afff72a9424
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2014-09-20 13:21:46 +0200

    try to converge to sunshine-master

--- reporter/src/org/metaborg/spt/listener/grading/TestRunner.java
+++ reporter/src/org/metaborg/spt/listener/grading/TestRunner.java
@@ -1,23 +1,15 @@
 package org.metaborg.spt.listener.grading;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PushbackInputStream;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.io.FileUtils;
-import org.metaborg.sunshine.Environment;
-import org.metaborg.sunshine.drivers.SunshineMainArguments;
 import org.metaborg.sunshine.drivers.SunshineMainDriver;
-import org.metaborg.sunshine.services.language.ALanguage;
+import org.metaborg.sunshine.environment.ServiceRegistry;
+import org.metaborg.sunshine.environment.SunshineMainArguments;
 import org.metaborg.sunshine.services.language.LanguageDiscoveryService;
-import org.metaborg.sunshine.services.language.LanguageService;
-import org.spoofax.interpreter.terms.IStrategoAppl;
-import org.spoofax.interpreter.terms.IStrategoTerm;
-import org.spoofax.terms.TermFactory;
-import org.spoofax.terms.io.binary.TermReader;
 
 public class TestRunner {
 
@@ -25,8 +17,7 @@ public class TestRunner {
 	private final File cache;
 	private final String builder;
 	private final SunshineMainArguments params = new SunshineMainArguments();
-	private final Environment env = Environment.INSTANCE();
-
+	
 	public TestRunner(File dir) throws ConfigurationException {
 
 		this.dir = dir;
@@ -49,38 +40,25 @@ public class TestRunner {
 		} catch (IOException e) {
 		}
 
-		env.setMainArguments(params);
-		env.setProjectDir(dir);
+		ServiceRegistry env = ServiceRegistry.INSTANCE();
+		env.reset();
+		
 		params.builder = builder;
 		params.filestobuildon = ".";
 		params.noanalysis = true;
-
+		params.project = dir.getPath();
+		
+		org.metaborg.sunshine.drivers.Main.initServices(env, params);
+		
 		return new SunshineMainDriver().run();
 	}
 
 	public static void registerLanguage(File esv) {
 
-		try {
-			PushbackInputStream input = new PushbackInputStream(
-					new FileInputStream(esv), 100);
-			byte[] buffer = new byte[6];
-			int bufferSize = input.read(buffer);
-			if (bufferSize != -1)
-				input.unread(buffer, 0, bufferSize);
+		LanguageDiscoveryService langDiscovery = ServiceRegistry.INSTANCE()
+				.getService(LanguageDiscoveryService.class);
+		assert langDiscovery != null;
+			langDiscovery.discover(esv.toPath());
 
-			if ((bufferSize == 6 && new String(buffer).equals("Module"))) {
-				TermReader reader = new TermReader(
-						new TermFactory()
-								.getFactoryWithStorageType(IStrategoTerm.MUTABLE));
-				IStrategoAppl document = (IStrategoAppl) reader
-						.parseFromStream(input);
-				ALanguage lang = LanguageDiscoveryService.INSTANCE()
-						.languageFromEsv(document,
-								esv.getParentFile().getParentFile().toPath());
-				LanguageService.INSTANCE().registerLanguage(lang);
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to load language", e);
-		}
 	}
 }
