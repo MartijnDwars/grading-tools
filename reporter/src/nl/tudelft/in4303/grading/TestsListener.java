d1a375ca5cb495241338ab02a5a5af848f2a2efc
Martijn Dwars <ikben@martijndwars.nl>
2015-09-13 14:13:54 +0200

    Signal test abortion from listener

--- reporter/src/nl/tudelft/in4303/grading/TestsListener.java
+++ reporter/src/nl/tudelft/in4303/grading/TestsListener.java
@@ -1,136 +1,133 @@
 package nl.tudelft.in4303.grading;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.List;
-
 import org.metaborg.spt.listener.ITestReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
 public class TestsListener implements ITestReporter {
-	private static final Logger logger = LoggerFactory.getLogger(TestsListener.class);
+    private static final Logger logger = LoggerFactory.getLogger(TestsListener.class);
 
-	private final HashSet<String> valid       = new HashSet<String>(500);
-	private final HashSet<String> invalid     = new HashSet<String>(500);
-	private final HashSet<String> effective   = new HashSet<String>(500);
-	private final HashSet<String> ineffective = new HashSet<String>(500);
+    private final HashSet<String> valid = new HashSet<>(500);
+    private final HashSet<String> invalid = new HashSet<>(500);
+    private final HashSet<String> effective = new HashSet<>(500);
+    private final HashSet<String> ineffective = new HashSet<>(500);
 
-	private final Hashtable<String, Integer> passed = new Hashtable<>();
-	private final Hashtable<String, Integer> missed = new Hashtable<>();
-	private final Hashtable<String, List<String>> descriptions = new Hashtable<>();
+    private final Hashtable<String, Integer> passed = new Hashtable<>();
+    private final Hashtable<String, Integer> missed = new Hashtable<>();
+    private final Hashtable<String, List<String>> descriptions = new Hashtable<>();
 
-	private boolean active = false;
-	private boolean detected = false;
-	private boolean init = false;
+    private boolean active = false;
+    private boolean detected = false;
+    private boolean init = false;
 
-	@Override
-	public void reset() throws Exception {
-	}
+    @Override
+    public void reset() throws Exception {
+    }
 
-	@Override
-	public void addTestsuite(String name, String filename) throws Exception {
-		passed.put(filename, 0);
-		missed.put(filename, 0);
-		descriptions.put(filename, new ArrayList<String>());
-//		if (active && detected) throw new RuntimeException();
-	}
+    @Override
+    public void addTestsuite(String name, String filename) throws Exception {
+        passed.put(filename, 0);
+        missed.put(filename, 0);
+        descriptions.put(filename, new ArrayList<String>());
+    }
 
-	@Override
-	public void addTestcase(String testsuiteFile, String description)
-			throws Exception {
-//		if (active && (detected || invalid.contains(testsuiteFile + " " + description))) throw new RuntimeException()
-	}
+    @Override
+    public void addTestcase(String testsuiteFile, String description) throws Exception {
+        // Nothing to do..
+    }
 
-	@Override
-	public void startTestcase(String testsuiteFile, String description)
-			throws Exception {
-	}
+    @Override
+    public void startTestcase(String testsuiteFile, String description) throws Exception {
+        // Nothing to do..
+    }
 
-	@Override
-	public void finishTestcase(String testsuiteFile, String description,
-			boolean succeeded, Collection<String> messages) throws Exception {
+    @Override
+    public boolean finishTestcase(String testsuiteFile, String description, boolean succeeded, Collection<String> messages) throws Exception {
+        if (!active) {
+            return false;
+        }
 
-		if (!active)
-			return;
+        if (succeeded) {
+            passed.put(testsuiteFile, passed.get(testsuiteFile) + 1);
+        } else {
+            missed.put(testsuiteFile, missed.get(testsuiteFile) + 1);
+            descriptions.get(testsuiteFile).add(description);
+        }
 
-		if (succeeded) {
-			passed.put(testsuiteFile, passed.get(testsuiteFile) + 1);
-		} else {
-			missed.put(testsuiteFile, missed.get(testsuiteFile) + 1);
-			descriptions.get(testsuiteFile).add(description);
-		}
-		final String key = testsuiteFile + " " + description;
+        final String key = testsuiteFile + " " + description;
 
-		if (init) {
-			if (succeeded) {
-				valid.add(key);
-				logger.trace("valid test {} in suite {}", description, testsuiteFile);
-			} else {
-				invalid.add(key);
-				logger.debug("invalid test {} in suite {}", description, testsuiteFile);
-			}
-		} else {
-			if (succeeded) {
-				logger.trace("ineffective test {} in suite {}", description, testsuiteFile);
-			} else if (valid.contains(key) && !invalid.contains(key)) {
-				ineffective.remove(key);
-				effective.add(key);
-				detected = true;
-				logger.debug("effective test {} in suite {}", description, testsuiteFile);
-			}
-		}
-	}
+        if (init) {
+            if (succeeded) {
+                valid.add(key);
+                logger.trace("valid test {} in suite {}", description, testsuiteFile);
+            } else {
+                invalid.add(key);
+                logger.debug("invalid test {} in suite {}", description, testsuiteFile);
+            }
+        } else {
+            if (succeeded) {
+                logger.trace("ineffective test {} in suite {}", description, testsuiteFile);
+            } else if (valid.contains(key) && !invalid.contains(key)) {
+                ineffective.remove(key);
+                effective.add(key);
+                detected = true;
+                logger.debug("effective test {} in suite {}", description, testsuiteFile);
 
-	public void init() {
-		active = true;
-		init = true;
-		valid.clear();
-		invalid.clear();
-		effective.clear();
-		ineffective.clear();
-	}
+                return false;
+            }
+        }
 
-	public boolean newLanguage() {
+        return true;
+    }
 
-		if (init) {
-			ineffective.addAll(valid);
-			init = false;
-			return true;
-		}
+    public void init() {
+        active = true;
+        init = true;
+        valid.clear();
+        invalid.clear();
+        effective.clear();
+        ineffective.clear();
+    }
 
-		boolean result = detected;
-		detected = false;
-		return result;
-	}
+    public boolean newLanguage() {
+        if (init) {
+            ineffective.addAll(valid);
+            init = false;
+            return true;
+        }
 
-	public int getValid() {
-		return valid.size();
-	}
+        boolean result = detected;
+        detected = false;
+        return result;
+    }
 
-	public int getInvalid() {
-		return invalid.size();
-	}
+    public int getValid() {
+        return valid.size();
+    }
 
-	public int getEffective() {
-		return effective.size();
-	}
+    public int getInvalid() {
+        return invalid.size();
+    }
 
-	public void exit() {
-		active = false;
-	}
+    public int getEffective() {
+        return effective.size();
+    }
 
-	public int getPassed(String spt) {
-		return passed.get(spt);
-	}
+    public void exit() {
+        active = false;
+    }
 
-	public int getMissed(String spt) {
-		return missed.get(spt);
-	}
+    public int getPassed(String spt) {
+        return passed.get(spt);
+    }
 
-	public List<String> getDescriptions(String spt) {
-		return descriptions.get(spt);
-	}
+    public int getMissed(String spt) {
+        return missed.get(spt);
+    }
+
+    public List<String> getDescriptions(String spt) {
+        return descriptions.get(spt);
+    }
 }
